1b12c3fe14625aa9d8d98994b2bc8afa44a599f0
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2025-05-23 15:15:21 +0300

    213

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,19 @@ import "./App.css";
 
 function App() {
   // @ts-expect-error: Telegram WebApp is injected globally
-  const tg = window.Telegram.WebApp;
+  const tg = window.Telegram.WebApp.initDataUnsafe.user;
 
   const [data, setData] = useState();
 
   useEffect(() => {
-    if (tg.initDataUnsafe.user) {
-      setData(tg.initDataUnsafe.user);
+    if (tg) {
+      setData(tg);
     }
   }, []);
 
   return (
     <>
-      <div>
-        {data ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Поле</th>
-                <th>Значение</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(data).map(([key, value]) => (
-                <tr key={key}>
-                  <td>{key}</td>
-                  <td>{JSON.stringify(value)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>Загрузка данных пользователя или информация недоступна.</p>
-        )}
-      </div>
+      <div>{data ? data : "Loading..."}</div>
     </>
   );
 }
